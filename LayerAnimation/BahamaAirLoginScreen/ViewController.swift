e7bd4e93f39610105b8fa6da10f9d5eafb1e0791
Jackson Ho <mintri1199@gmail.com>
2019-06-08 17:47:30 -0700

    [Modified] Finished Chapter 9

--- LayerAnimation/BahamaAirLoginScreen/ViewController.swift
+++ LayerAnimation/BahamaAirLoginScreen/ViewController.swift
@@ -49,7 +49,7 @@ class ViewController: UIViewController {
     let messages = ["Connecting ...", "Authorizing ...", "Sending credentials ...", "Failed"]
     
     var statusPosition = CGPoint.zero
-    
+    let info = UILabel()
     // MARK: view controller methods
     
     override func viewDidLoad() {
@@ -75,6 +75,14 @@ class ViewController: UIViewController {
         status.addSubview(label)
         
         statusPosition = status.center
+        
+        info.frame = CGRect(x: 0, y: loginButton.center.y + 60, width: view.frame.size.width, height: 30)
+        info.backgroundColor = .clear
+        info.font = UIFont(name: "HelveticaNeue", size: 12.0)
+        info.textAlignment = .center
+        info.textColor = .white
+        info.text = "Tap on a field and enter username and password"
+        view.insertSubview(info, belowSubview: loginButton)
     }
     
     func tintBackgroundColor(layer: CALayer, toColor: UIColor) {
@@ -125,18 +133,22 @@ class ViewController: UIViewController {
         flyRight.toValue = view.bounds.size.width / 2
         flyRight.duration = 0.5
         flyRight.fillMode = .both
-        
+        flyRight.delegate = self
+        flyRight.setValue("form", forKey: "name")
+        flyRight.setValue(heading.layer, forKey: "layer")
         heading.layer.add(flyRight, forKey: nil)
+        
         // Create an offset for the username field
         flyRight.beginTime = CACurrentMediaTime() + 0.3
+        flyRight.setValue(username.layer, forKey: "layer")
         username.layer.add(flyRight, forKey: nil)
         username.layer.position.x = view.bounds.size.width / 2
         
+        // Create an offset for the password field
         flyRight.beginTime = CACurrentMediaTime() + 0.4
+        flyRight.setValue(password.layer, forKey: "layer")
         password.layer.add(flyRight, forKey: nil)
         password.layer.position.x = view.bounds.size.width / 2
-        
-        
         
         
         UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 0.5,
@@ -148,10 +160,25 @@ class ViewController: UIViewController {
                        completion: nil
         )
         
-        animateCloud(cloud1)
-        animateCloud(cloud2)
-        animateCloud(cloud3)
-        animateCloud(cloud4)
+        animateCloud(layer: cloud1.layer)
+        animateCloud(layer: cloud2.layer)
+        animateCloud(layer: cloud3.layer)
+        animateCloud(layer: cloud4.layer)
+        
+        let flyLeft = CABasicAnimation(keyPath: "position.x")
+        flyLeft.fromValue = info.layer.position.x + view.frame.size.width
+        flyLeft.toValue = info.layer.position.x
+        flyLeft.duration = 5
+        info.layer.add(flyLeft, forKey: "infoappear")
+        
+        let fadeLabelIn = CABasicAnimation(keyPath: "opacity")
+        fadeLabelIn.fromValue = 0.2
+        fadeLabelIn.toValue = 1
+        fadeLabelIn.duration = 4.5
+        info.layer.add(fadeLabelIn, forKey: "fadein")
+        
+        username.delegate = self
+        password.delegate = self
     }
     
     func showMessage(index: Int) {
@@ -243,18 +270,18 @@ class ViewController: UIViewController {
         roundCorner(layer: loginButton.layer, toRadius: 25)
     }
     
-    func animateCloud(_ cloud: UIImageView) {
-        let cloudSpeed = 60.0 / view.frame.size.width
-        let duration = (view.frame.size.width - cloud.frame.origin.x) * cloudSpeed
-        UIView.animate(withDuration: TimeInterval(duration), delay: 0.0, options: .curveLinear,
-                       animations: {
-                        cloud.frame.origin.x = self.view.frame.size.width
-        },
-                       completion: {_ in
-                        cloud.frame.origin.x = -cloud.frame.size.width
-                        self.animateCloud(cloud)
-        }
-        )
+    func animateCloud(layer: CALayer) {
+        let cloudSpeed = 4.0 / Double(view.layer.frame.size.width)
+        
+        let duration: TimeInterval = Double(view.layer.frame.size.width - layer.frame.origin.x) * cloudSpeed
+        
+        let cloudMove = CABasicAnimation(keyPath: "position.x")
+        cloudMove.duration = duration
+        cloudMove.toValue = self.view.bounds.width + layer.bounds.width / 2
+        cloudMove.delegate = self
+        cloudMove.setValue("cloud", forKey: "name")
+        cloudMove.setValue(layer, forKey: "layer")
+        layer.add(cloudMove, forKey: nil)
     }
     
     // MARK: UITextFieldDelegate
@@ -265,4 +292,39 @@ class ViewController: UIViewController {
         return true
     }
     
+}
+extension ViewController: CAAnimationDelegate {
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        guard let name = anim.value(forKey: "name") as? String else { return }
+        
+        if name == "form" {
+            let layer = anim.value(forKey: "layer") as? CALayer
+            anim.setValue(nil, forKey: "layer")
+            
+            let pulse = CABasicAnimation(keyPath: "transform.scale")
+            pulse.fromValue = 1.25
+            pulse.toValue = 1
+            pulse.duration = 0.25
+            layer?.add(pulse, forKey: nil)
+        } else if name == "cloud" {
+            guard let layer = anim.value(forKey: "layer") as? CALayer else { return }
+            layer.setValue(view.bounds.width, forKey: "position.x")
+            
+            delay(seconds: 0.5) {
+                self.animateCloud(layer: layer)
+            }
+        }
+        print("animation did finish")
+    }
+}
+// MARK: UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        guard let runningAnimation = info.layer.animationKeys() else {
+            return
+        }
+        
+        print(runningAnimation)
+        info.layer.removeAnimation(forKey: "infoappear")
+    }
 }
