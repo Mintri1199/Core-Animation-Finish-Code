e797a4ec45f578d93d7a490dbb90e51284567424
Jackson Ho <mintri1199@gmail.com>
2019-06-24 14:37:09 -0700

    [Modified] Made progress on Chapter 15, completed Iris speak animation

--- Iris/Iris/ViewController.swift
+++ Iris/Iris/ViewController.swift
@@ -24,38 +24,129 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-  
-  @IBOutlet weak var meterLabel: UILabel!
-  @IBOutlet weak var speakButton: UIButton!
-  
-  let monitor = MicMonitor()
-  let assistant = Assistant()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
+    @IBOutlet weak var meterLabel: UILabel!
+    @IBOutlet weak var speakButton: UIButton!
     
-  }
-  
-  @IBAction func actionStartMonitoring(_ sender: AnyObject) {
+    let monitor = MicMonitor()
+    let assistant = Assistant()
+    let replicator = CAReplicatorLayer()
+    let dot = CALayer()
+    let dotLength: CGFloat = 6.0
+    let dotOffSet: CGFloat = 8.0
     
-  }
-  
-  @IBAction func actionEndMonitoring(_ sender: AnyObject) {
-    
-    //speak after 1 second
-    delay(seconds: 1.0) {
-      self.startSpeaking()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        replicator.frame = view.bounds
+        view.layer.addSublayer(replicator)
+        
+        dot.frame = CGRect(
+            x: replicator.frame.size.width - dotLength,
+            y: replicator.position.y,
+            width: dotLength, height: dotLength)
+        
+        dot.backgroundColor = UIColor.lightGray.cgColor
+        dot.borderColor = UIColor(white: 1, alpha: 1).cgColor
+        dot.borderWidth = 0.5
+        dot.cornerRadius = 1.5
+        replicator.addSublayer(dot)
+        replicator.instanceCount = Int(view.frame.size.width / dotOffSet)
+        replicator.instanceTransform = CATransform3DMakeTranslation(-dotOffSet, 0, 0)
+        
+        // This is a test animation, you're going to delete it
+//        let move = CABasicAnimation(keyPath: "position.y")
+//        move.fromValue = dot.position.y
+//        move.toValue = dot.position.y - 50
+//        move.duration = 1
+//        move.repeatCount = 10
+//        move.autoreverses = true
+//        dot.add(move, forKey: nil)
+        replicator.instanceDelay = 0.02
     }
-  }
-  
-  func startSpeaking() {
-    print("speak back")
     
+    @IBAction func actionStartMonitoring(_ sender: AnyObject) {
+        
+    }
     
-  }
-  
-  func endSpeaking() {
+    @IBAction func actionEndMonitoring(_ sender: AnyObject) {
+        
+        //speak after 1 second
+        delay(seconds: 1.0) {
+            self.startSpeaking()
+        }
+    }
     
-  }
+    func startSpeaking() {
+        print("speak back")
+        meterLabel.text = assistant.randomAnswer()
+        assistant.speak(meterLabel.text!, completion: endSpeaking)
+        speakButton.isHidden = true
+        
+        let scale = CABasicAnimation(keyPath: "transform")
+        scale.fromValue = NSValue(caTransform3D: CATransform3DIdentity)
+        scale.toValue = NSValue(caTransform3D: CATransform3DMakeScale(1.4, 15, 1))
+        scale.duration = 0.33
+        scale.repeatCount = .infinity
+        scale.autoreverses = true
+        scale.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        dot.add(scale, forKey: "dotScale")
+        
+        let fade = CABasicAnimation(keyPath: "opacity")
+        fade.fromValue = 1
+        fade.toValue = 0.2
+        fade.duration = 0.33
+        fade.beginTime = CACurrentMediaTime() + 0.33
+        fade.repeatCount = .infinity
+        fade.autoreverses = true
+        fade.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        dot.add(fade, forKey: "dotOpacity")
+        
+        let tint = CABasicAnimation(keyPath: "backgroundColor")
+        tint.fromValue = UIColor.magenta.cgColor
+        tint.toValue = UIColor.cyan.cgColor
+        tint.duration = 0.66
+        tint.beginTime = CACurrentMediaTime() + 0.28
+        tint.fillMode = .backwards
+        tint.repeatCount = .infinity
+        tint.autoreverses = true
+        tint.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        dot.add(tint, forKey: "dotColor")
+        
+        let initialRotation = CABasicAnimation(keyPath:
+            "instanceTransform.rotation")
+        initialRotation.fromValue = 0.0
+        initialRotation.toValue   = 0.01
+        initialRotation.duration = 0.33
+        initialRotation.isRemovedOnCompletion = false
+        initialRotation.fillMode = .forwards
+        initialRotation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        replicator.add(initialRotation, forKey: "initialRotation")
+        
+        let rotation = CABasicAnimation(keyPath: "instanceTransform.rotation")
+        rotation.fromValue = 0.01
+        rotation.toValue = -0.01
+        rotation.duration = 0.99
+        rotation.beginTime = CACurrentMediaTime() + 0.33
+        rotation.repeatCount = .infinity
+        rotation.autoreverses = true
+        rotation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+        replicator.add(rotation, forKey: "replicatorRotation")
+        
+    }
+    
+    func endSpeaking() {
+        replicator.removeAllAnimations()
+        let scale = CABasicAnimation(keyPath: "transform")
+        scale.toValue = NSValue(caTransform3D: CATransform3DIdentity)
+        scale.duration = 0.33
+        scale.isRemovedOnCompletion = false
+        scale.fillMode = .forwards
+        dot.add(scale, forKey: nil)
+        
+        dot.removeAnimation(forKey: "dotColor")
+        dot.removeAnimation(forKey: "dotOpacity")
+        dot.backgroundColor = UIColor.lightGray.cgColor
+        speakButton.isHidden = false
+        
+    }
 }
