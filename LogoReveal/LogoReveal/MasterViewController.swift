0ac0692534dc1009f445e39f67bc47d1b9b8ead4
Jackson Ho <mintri1199@gmail.com>
2019-06-26 16:34:22 -0700

    [Modified] Finished Chapter 18

--- LogoReveal/LogoReveal/MasterViewController.swift
+++ LogoReveal/LogoReveal/MasterViewController.swift
@@ -1,24 +1,24 @@
 /*
-* Copyright (c) 2014-present Razeware LLC
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*/
+ * Copyright (c) 2014-present Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import UIKit
 import QuartzCore
@@ -30,11 +30,12 @@ func delay(seconds: Double, completion: @escaping ()-> Void) {
 class MasterViewController: UIViewController {
   
   let logo = RWLogoLayer.logoLayer()
-
+  let transition = RevealAnimator()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     title = "Start"
-
+    navigationController?.delegate = self
   }
   
   override func viewDidAppear(_ animated: Bool) {
@@ -46,7 +47,7 @@ class MasterViewController: UIViewController {
     
     // add the logo to the view
     logo.position = CGPoint(x: view.layer.bounds.size.width/2,
-      y: view.layer.bounds.size.height/2 - 30)
+                            y: view.layer.bounds.size.height/2 - 30)
     logo.fillColor = UIColor.white.cgColor
     view.layer.addSublayer(logo)
   }
@@ -60,3 +61,8 @@ class MasterViewController: UIViewController {
   
 }
 
+extension MasterViewController: UINavigationControllerDelegate {
+  func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    return transition
+  }
+}
